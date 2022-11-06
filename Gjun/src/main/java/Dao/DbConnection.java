b7b000214565bc7a6fb7dfa4d5ec080493ef0859
zxc <zxc>
2022-11-06 16:21:28 +0800

    GJUN-20221106

---
+++ Gjun/src/main/java/Dao/DbConnection.java
@@ -0,0 +1,18 @@
+package Dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DbConnection {
+
+	public static void main(String[] args) {
+		System.out.print(DbConnection.getDb());
+
+	}
+	public static EntityManager getDb() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Gjun");
+		EntityManager em = emf.createEntityManager();
+		return em;
+	}
+}
---
+++ Gjun/src/main/webapp/member/addMemberSuccess.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=BIG5"
+    pageEncoding="BIG5"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="BIG5">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>註冊成功</h2>
+<a href="../index.jsp">返回登入</a>
+</body>
+</html>
